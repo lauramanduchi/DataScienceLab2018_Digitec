01cdf23d5eae5b1d43de4ac6c86a867e6128c163
Laura Manduchi <lauraman@student.ethz.ch>
2018-10-23 22:28:47 +0200

    Debugging

--- code/MaxMI_Algo.py
+++ code/MaxMI_Algo.py
@@ -8,24 +8,26 @@ import random
 import numpy as np
 import pandas as pd
 from utils import *
+from init_dataframe import init_df
+import algo_utils
 
 
 '''Normalized (# products given answer/total # products given question)'''
 def prob_answer(answer, question, product_set, traffic_set):
-    p_answer = get_proba_Q_distribution(question, product_set, traffic_set, alpha=1) #Mel function name
+    p_answer = algo_utils.get_proba_Q_distribution(question, product_set, traffic_set, alpha=1) #Mel function name
     return p_answer.loc[answer]["final_proba"]
 
 
 '''Normalized (# products y bought/total #products bought) + (# products y /total #products)'''
 def prob_product(product, product_set, purchased_set):
-    p_product = get_proba_Y_distribution(product_set, purchased_set, alpha=1) #Mel function name
+    p_product = algo_utils.get_proba_Y_distribution(product_set, purchased_set, alpha=1) #Mel function name
     return p_product.loc[product]["final_proba"]
 
 '''
 Updates product_set, traffic_set and purchased_set to account for conditionality
 '''
 def conditional_entropy(question, answer, product_set, traffic_set, purchased_set):
-    product_set, traffic_set, purchased_set = select_subset(question=question, answer=answer,
+    product_set, traffic_set, purchased_set = algo_utils.select_subset(question=question, answer=answer,
                                                             product_set=product_set, traffic_set =traffic_set,
                                                             purchased_set = purchased_set)
     product_ids = product_set["ProductId"].drop_duplicates().values
@@ -54,53 +56,43 @@ def opt_step(question_set, product_set, traffic_set):
     next_question = np.argmax(MI_matrix, axis=0)[1]
     return next_question
 
+def get_distinct_products(product_set):
+    try:
+        distinct_p = product_set.ProductId.unique()
+    except AttributeError:
+        print("'ProductId' is not a valid column in Product_set, rename it!")
+    return distinct_p
+
 
 '''Our algorith which returns:
  1) sequence of question to ask
  2) final product list
  3) y chosen as input of algo'''
 def max_info_algorithm(product_set, traffic_set, threshold, y):
-    question_set = get_questions(product_set) # TODO
-    quest_answer_y = get_answers_y(y, product_set) # TODO
+    question_set = algo_utils.get_questions(product_set)
+    quest_answer_y = algo_utils.get_answers_y(y, product_set)
 
     final_question_list=[]
 
-    while len(product_set) < threshold: #attention number of different products
+    distinct_products = get_distinct_products(product_set)   #laura
+
+    while not (len(distinct_products) < threshold or len(question_set) == 0):     #laura
         next_question = opt_step(question_set, product_set, traffic_set)
         final_question_list.append[next_question]
-        answer = quest_answer_y[next_question] # TODO
-        product_set, traffic_set, _ = select_subset(question=question, answer=answer, product_set=product_set,
+        answer = quest_answer_y[str(next_question)][0]          #laura
+        product_set, traffic_set, _ = algo_utils.select_subset(question=question, answer=answer, product_set=product_set,
                                                      traffic_set =traffic_set, purchased_set = purchased_set)
-        question_set = get_filters_remaining(product_set)
+        question_set = algo_utils.get_filters_remaining(product_set)
+        distinct_products = get_distinct_products(product_set) #laura
 
     return final_question_list, product_set, y
 
 
 
 '''Testing'''
-# Loading question_dic from dataset - downloaded data for category 6 from db
-filters_def_dict = load_obj('filters_def_dict')
-question_set = list(filters_def_dict.keys())
-print(len(question_set))
-print("Question set: {}".format(list(question_set)))
-
-
-# Partial dataset for category 6 - downloaded data for category 6 from db
-df = pd.read_csv('category6_select_subset.csv')
-random.seed(99)
-df = df.iloc[1:100]
-purchased_set=None
-print(df['ProductId'].value_counts(normalize=False).keys())
-print(list(df))
-
-y = 5820741
-
-# Checking that all probabilities are one
-total_prob=0
-for y in df['ProductId'].value_counts(normalize=False).keys():
-    total_prob += prob_product(y, df, purchased_set)
-print(total_prob)
-print(df['ProductId'].nunique())
-
-# Get question set (from mel's Get filters code)
-filters_def_dict.keys()
+products_cat, traffic_cat = init_df()
+y = products_cat
+threshold = 50
+final_question_list, product_set, y = max_info_algorithm(product_cat, traffic_cat, threshold, y)
+print("final_question_list: ", final_question_list)
+print("length final product set: ", len(product_set))
