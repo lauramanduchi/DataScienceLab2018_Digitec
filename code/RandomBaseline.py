3e2984619af3fd853f79be97aa68699d48ba92db
Mélanie Bernhardt <32590828+melanibe@users.noreply.github.com>
2018-10-30 10:36:14 +0100

    Update RandomBaseline.py

--- code/RandomBaseline.py
+++ code/RandomBaseline.py
@@ -5,7 +5,6 @@ from load_utils import *
 import random
 import numpy as np
 import pandas as pd
-#from utils import *
 from init_dataframes import init_df
 import algo_utils
 
@@ -25,21 +24,18 @@ def get_distinct_products(product_set):
  3) y chosen as input of algo'''
 def random_baseline(product_set, traffic_set, purchased_set, threshold, y):
     question_set = set(algo_utils.get_questions(product_set))
-    print(question_set)
     quest_answer_y = algo_utils.get_answers_y(y, product_set)
     final_question_list=[]
-
-    distinct_products = get_distinct_products(product_set)   #laura
-
-    while not (len(distinct_products) < threshold or len(question_set) == 0):     #laura
+    distinct_products = get_distinct_products(product_set)   
+    while not (len(distinct_products) < threshold or len(question_set) == 0):   
         next_question = np.random.choice(np.asarray(list(question_set)), size=1)[0]
         final_question_list.append(int(next_question))
-        answer = quest_answer_y[int(next_question)]          #laura
+        answer = quest_answer_y[int(next_question)]     
         product_set, traffic_set, purchased_set = algo_utils.select_subset(question=int(next_question), answer=str(answer), product_set=product_set,traffic_set =traffic_set, purchased_set = purchased_set)
         question_set_new = set(algo_utils.get_filters_remaining(product_set))
         question_set = question_set_new.difference(final_question_list) # s- t is written s.difference(t)
-        distinct_products = get_distinct_products(product_set) #laura
-        print(len(get_distinct_products(product_set)))
+        distinct_products = get_distinct_products(product_set) 
+        print('There are still {} products to choose from'.format(len(get_distinct_products(product_set))))
     return final_question_list, product_set, y
 
 
