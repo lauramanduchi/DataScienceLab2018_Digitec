f51e1747e607e17f4bc43e511f881adafe10917d
Mélanie Bernhardt <32590828+melanibe@users.noreply.github.com>
2018-10-30 10:35:23 +0100

    Add files via upload

---
+++ code/eliminate.py
@@ -0,0 +1,107 @@
+from load_utils import *
+import random
+import numpy as np
+import pandas as pd
+from init_dataframes import init_df
+import algo_utils
+import parmap
+import time
+
+# about parmap 
+#y = [myfunction(x, argument1, mykeyword=argument2) for x in mylist]
+# In parallel:
+#y = parmap.map(myfunction, mylist, argument1, mykeyword=argument2)
+
+
+
+# IDEAS TO IMPROVE
+# we can weight the filter by history usage (to avoid having bad filters as first questions)
+# take our ideas from the first baseline
+# check how idk is handled in get proba Q
+
+def expectation_eliminate(question, product_set, traffic_cat): 
+    proba_Q = algo_utils.get_proba_Q_distribution_idk(question, product_set, traffic_cat)
+    product_q = algo_utils.select_subset(product_set, question=question)[0]
+    possible_answers = proba_Q.index
+    proba_Q["eliminate"]=0
+    for answer in possible_answers:
+        tmp = product_q.loc[product_q["answer"].astype(str)==str(answer)]
+        proba_Q.loc[answer, "eliminate"] = 1-len(get_distinct_products(tmp))/len(get_distinct_products(product_set))
+        # 1 - prop_kept = prop_eliminate
+    proba_Q.loc["idk", "eliminate"] = 0 # if answer is idk eliminate prop is 0
+    prop = np.sum(proba_Q["final_proba"]*proba_Q["eliminate"])
+    #print(prop)
+    return (prop)
+
+# Return question which maximizes eliminate
+def opt_step(question_set, product_set, traffic_set, purchased_set):
+    MI_matrix = np.zeros([len(question_set), 2])
+    mutual_array = np.asarray(parmap.map(expectation_eliminate, \
+                                        question_set,\
+                                         product_set=product_set,\
+                                          traffic_cat=traffic_set, \
+                                          pm_pbar=True, \
+                                          pm_parallel=True))
+    MI_matrix[:,0] = list(question_set)
+    MI_matrix[:,1] = mutual_array
+    next_question_index = np.argmax(MI_matrix, axis=0)[1]
+    next_question = MI_matrix[next_question_index, 0]
+    print(next_question)
+    return next_question
+
+def get_distinct_products(product_set):
+    try:
+        distinct_p = product_set.ProductId.unique()
+    except AttributeError:
+        print("'ProductId' is not a valid column in Product_set, rename it!")
+    return distinct_p
+
+
+'''Our algorith which returns:
+ 1) sequence of question to ask
+ 2) final product list
+ 3) y chosen as input of algo'''
+
+ ### BE CAREFUL IT IS SUPER IMPORTANT TO TAKE IDK ANSWERS INTO ACCOUNT IN THE Q DISTRIBUTION
+ ### WE SHOULD MODIDY THE OTHER ALGORITHM TO TAKE THAT INTO ACCOUNT
+def max_eliminate_algorithm(product_set, traffic_set, purchased_set, threshold, y):
+    question_set = set(algo_utils.get_questions(product_set))
+    quest_answer_y = algo_utils.get_answers_y(y, product_set)
+    final_question_list=[]
+    distinct_products = get_distinct_products(product_set)  
+    print("There are {} questions we can ask".format(len(question_set)))
+    print("There are {} possible products to choose from".format(len(distinct_products)))
+    iter = 1
+    while not (len(distinct_products) < threshold or len(question_set) == 0):     #laura
+        next_question = opt_step(question_set, product_set, traffic_set, purchased_set)
+        print("Next question is filter : {}".format(next_question))
+        final_question_list.append(int(next_question))
+        answer = quest_answer_y[int(next_question)]        
+        product_set, traffic_set, purchased_set = algo_utils.select_subset(question=next_question, answer=answer, product_set=product_set, traffic_set =traffic_set, purchased_set = purchased_set)
+        question_set_new = set(algo_utils.get_filters_remaining(product_set)) 
+        question_set = question_set_new.difference(final_question_list)
+        distinct_products = get_distinct_products(product_set)
+        print("There are {} possible products to choose from".format(len(get_distinct_products(product_set))))
+        iter+=1
+    return final_question_list, product_set, y
+
+
+if __name__=='__main__':
+    try:
+        products_cat = load_obj('../data/products_table')
+        traffic_cat = load_obj('../data/traffic_table')
+        purchased_cat = load_obj('../data/purchased_table')
+        print("Loaded datsets")
+    except:
+        print("Creating datasets...")
+        products_cat, traffic_cat, purchased_cat = init_df()
+    
+    y = products_cat["ProductId"][10]
+    threshold = 50
+    start_time = time.time()
+    print("Start time: {}".format(start_time))
+    final_question_list, product_set, y = max_eliminate_algorithm(products_cat, traffic_cat, purchased_cat, threshold, y)
+    end_time = time.time()
+    print("final_question_list: ", final_question_list)
+    print("length final product set: ", len(product_set))
+    print("The algorithm took {}s.".format(end_time-start_time))
---
+++ code/evaluation_eliminate.py
@@ -0,0 +1,63 @@
+import time
+import os
+import numpy as np
+from load_utils import *
+from eliminate import max_eliminate_algorithm
+from RandomBaseline import random_baseline, get_distinct_products
+import pandas as pd
+import logging 
+from init_dataframes import init_df
+import algo_utils
+try:
+    products_cat = load_obj('../data/products_table')
+    traffic_cat = load_obj('../data/traffic_table')
+    purchased_cat = load_obj('../data/purchased_table')
+    print("Loaded datsets")
+except:
+    print("Creating datasets...")
+    products_cat, traffic_cat, purchased_cat = init_df()
+
+time.time()
+t = time.strftime('%d%b%y_%H%M%S')
+cwd = os.getcwd()
+checkpoint_dir = cwd+'/../runs_eliminate/' + t + '/'
+os.makedirs(checkpoint_dir, 0o777)
+print('Saving to ' + checkpoint_dir)
+
+size_test = 25    
+y_array = np.random.choice(products_cat["ProductId"].drop_duplicates().values, size = size_test)
+threshold = 50
+length_opt = []
+length_rdm = []
+opt_quest = []
+rdm_quest = []
+for y in y_array:
+    final_question_list, product_set, y = max_eliminate_algorithm(products_cat, traffic_cat, purchased_cat, threshold, y)
+    print('the length of optimal eliminate filter was {}'.format(len(final_question_list)))
+    length_opt.append(len(final_question_list))
+    opt_quest.append(final_question_list)
+    final_question_list, product_set, y = random_baseline(products_cat, traffic_cat, purchased_cat, threshold, y)
+    length_rdm.append(len(final_question_list))
+    print('the length of random filter was {}'.format(len(final_question_list)))
+    rdm_quest.append(final_question_list)
+
+res = pd.DataFrame()
+res["random"] = length_rdm
+res["opt"] = length_opt
+res.to_csv(checkpoint_dir +'/lengths.csv', header = True, index = False)
+
+quest = pd.DataFrame()
+quest["random"] = rdm_quest
+quest["opt"] = opt_quest
+quest.to_csv(checkpoint_dir +'/quest.csv', header = True, index = False)
+
+with open(checkpoint_dir +'/summary.txt', 'w+') as f:
+    f.write('Avg number of questions for optimal {} \n'.format(np.mean(np.asarray(length_opt))))
+    f.write('Std number of questions for optimal {} \n'.format(np.std(np.asarray(length_opt))))
+    f.write('Max number of questions for optimal {} \n'.format(np.max(np.asarray(length_opt))))
+    f.write('Min number of questions for optimal {} \n'.format(np.min(np.asarray(length_opt))))
+    f.write('\n')
+    f.write('Avg number of questions for random {} \n'.format(np.mean(np.asarray(length_rdm))))
+    f.write('Std number of questions for random {} \n'.format(np.std(np.asarray(length_rdm))))
+    f.write('Max number of questions for random {} \n'.format(np.max(np.asarray(length_rdm))))
+    f.write('Min number of questions for random {} \n'.format(np.min(np.asarray(length_rdm))))
