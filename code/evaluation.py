04bf97bce2f53d2678bf0448ed3e19571845bef5
melanibe <melanibe@student.ethz.ch>
2018-12-03 16:22:25 +0100

    unique file for evaluation

--- code/evaluation.py
+++ code/evaluation.py
@@ -11,17 +11,22 @@ import argparse
 import warnings
 
 from utils.algo_utils import get_proba_Y_distribution
-from utils.load_utils import *
+from utils.load_utils import load_obj, save_obj
 from utils.init_dataframes import init_df
 import utils.algo_utils as algo_utils
 from utils.sampler import sample_answers
 from greedy.MaxMI_Algo import max_info_algorithm, opt_step
 from greedy.RandomBaseline import random_baseline
+from dagger.evaluation import dagger_get_questions
+from dagger.dagger_utils import get_products
+from utils.build_answers_utils import question_id_to_text, answer_id_to_text
+import dagger.dagger_utils as dagger_utils
+from dagger.model import create_model
 
 """ This module runs the evaluation of MaxMI.
 For each target product in the test set.
     * Sample the answers
-    * Find the optimal list of questions with MaxMI
+    * Find the optimal list of questions with MaxMI or Dagger
     * Find the list of questions with randomBaseline
     * Save the asked questions
     * Output summary statistics of evaluation
@@ -48,6 +53,8 @@ parser.add_argument("-p2a", "--p2a",
                     help="proba of user giving 2 answers to a question", type=float)
 parser.add_argument("-p3a", "--p3a",
                     help="proba of user giving 3 answers to a question", type=float)
+parser.add_argument("-r", "--run", help="checkpoint subfolder name", type=str)
+parser.add_argument("-algo", "--algo", help="which algo to evalute choose 'MaxMI' or 'Dagger'", type=str)
 
 args = parser.parse_args()
 size_test = args.size if args.size else 25
@@ -55,10 +62,16 @@ a_hist = args.a_hist if args.a_hist else 0.0
 p_idk = args.pidk if args.pidk else 0.0
 p_2a = args.p2a if args.p2a else 0.0
 p_3a = args.p3a if args.p3a else 0.0
+run = args.run if args.run else 'default'
+use = args.algo if args.algo else 'maxMI'
 
+print('Using {} algorithm'.format(use))
+if use =='maxMI':
+    suffix = 'pidk{}_p2a{}_p3a{}_hist{}_s{}_t{}'.format(p_idk,p_2a,p_3a,a_hist,size_test,t)
+else:
+    suffix = 'pidk{}_p2a{}_p3a{}_s{}_t{}'.format(p_idk,p_2a,p_3a,size_test,t)
 
-suffix = 'pidk{}_p2a{}_p3a{}_hist{}_s{}_t{}'.format(p_idk,p_2a,p_3a,a_hist,size_test,t)
-checkpoint_dir = cwd+'/../runs_MaxMI/'  + suffix + '/'
+checkpoint_dir = cwd+'/../evaluation_{}/'.format(use)  + suffix + '/'
 os.makedirs(checkpoint_dir, 0o777)
 print('Saving to ' + checkpoint_dir)
 
@@ -73,6 +86,7 @@ try:
     purchased_cat = load_obj('../data/purchased_table')
     question_text_df = load_obj('../data/question_text_df')
     answer_text_df = load_obj('../data/answer_text')
+    filters_def_dict = load_obj('../data/filters_def_dict')
     print("Loaded datasets")
 except:
     print("Creating datasets...")
@@ -87,7 +101,7 @@ except:
     print("Created datasets")
 
 df_history = 0
-if a_hist > 0:
+if a_hist > 0 and use=='MaxMI':
     try:
         df_history = load_obj('../data/df_history')
     except:
@@ -97,10 +111,16 @@ if a_hist > 0:
 
 
 # ============= INIT VARIABLES ========== #
-with open(checkpoint_dir +'/parameters.txt', 'w+') as f:
-    f.write('Test set size: {} \n Probability of answering I dont know: {} \n Probability of giving 2 answers: {} Probability of giving 3 answers: {} \n'.format(size_test, p_idk, p_2a, p_3a))
-    f.write('Alpha parameter: {}'.format(a_hist))
-
+if use=='maxMI':
+    with open(checkpoint_dir +'/parameters.txt', 'w+') as f:
+        f.write('Test set size: {} \n Probability of answering I dont know: {} \n Probability of giving 2 answers: {} Probability of giving 3 answers: {} \n'.format(size_test, p_idk, p_2a, p_3a))
+        f.write('Alpha parameter: {}'.format(a_hist))
+else:
+    model_dir = '../runs/{}/'.format(run) #h1256_h2128_ts1543838439
+    checkpoint_model = model_dir+'/cp.ckpt' 
+    with open(checkpoint_dir +'/parameters.txt', 'w+') as f:
+        f.write('Test set size: {} \n Probability of answering I dont know: {} \n Probability of giving 2 answers: {} Probability of giving 3 answers: {} \n'.format(size_test, p_idk, p_2a, p_3a))
+        f.write('Loading the latest model from {}'.format(checkpoint_model))   
 #probabilities of products given traffic
 p_y = get_proba_Y_distribution(products_cat, purchased_cat, alpha=1)["final_proba"].values
 y_array = np.random.choice(products_cat["ProductId"].drop_duplicates().values, size = size_test, p = p_y)
@@ -115,32 +135,40 @@ opt_answer_text_list = []
 rdm_answer_text_list = []
 opt_prod_nb = []
 rdm_prod_nb = []
+
 with open(checkpoint_dir +'/lengths.csv', 'w+') as f:
-    f.write("MaxMI, random \n")
+    f.write("{}, Random \n".format(use))
 with open(checkpoint_dir +'/rdm.csv', 'w+') as f:
     f.write("QuestionId, QuestionText, AnswerText \n")
-with open(checkpoint_dir +'/optimal.csv', 'w+') as f:
+with open(checkpoint_dir +'/{}.csv'.format(use), 'w+') as f:
     f.write("QuestionId, QuestionText, AnswerText \n")
 
+if use=='maxMI':
+    # Optimization: compute first_questions outside product loop
+    first_questions = []
+    first_question_set = set(algo_utils.get_questions(products_cat))
+    n_first_q = 3 
+    print("Optimization: computing first {} questions without history beforehand".format(n_first_q))
+    for i in range(n_first_q):
+        first_question = opt_step(first_question_set, products_cat, traffic_cat, purchased_cat, a_hist, df_history)
+        first_questions.append(first_question)
+        first_question_set = first_question_set.difference(set(first_questions))
 
-# Optimization: compute first_questions outside product loop
-first_questions = []
-first_question_set = set(algo_utils.get_questions(products_cat))
-n_first_q = 3 
-print("Optimization: computing first {} questions without history beforehand".format(n_first_q))
-for i in range(n_first_q):
-    first_question = opt_step(first_question_set, products_cat, traffic_cat, purchased_cat, a_hist, df_history)
-    first_questions.append(first_question)
-    first_question_set = first_question_set.difference(set(first_questions))
-
+else:
+    print('Loading the latest model from {}'.format(checkpoint_model))
+    length_state = len(dagger_utils.get_onehot_state({}, filters_def_dict))
+    number_filters = len(filters_def_dict.keys())
+    model = create_model(number_filters, length_state, h1=128, h2=122)
+    model.load_weights(checkpoint_model)
 
 # ============= MAIN EVALUATION LOOP =============== #
 for y in y_array:
     print(y)
     # Sample answers
     answers_y = sample_answers(y, products_cat, p_idk = p_idk, p_2a = p_2a, p_3a = p_3a)
-    # Compute optimal list with MaxMI
-    final_question_list, product_set, y, final_question_text_list, answer_text_list = max_info_algorithm(products_cat, 
+    if use=='maxMI':
+        # Compute optimal list with MaxMI
+        final_question_list, product_set, y, final_question_text_list, answer_text_list = max_info_algorithm(products_cat, 
                                                                                                          traffic_cat, 
                                                                                                          purchased_cat,
                                                                                                          question_text_df,
@@ -151,8 +179,17 @@ for y in y_array:
                                                                                                          a_hist,
                                                                                                          df_history,
                                                                                                          first_questions)
+    else:
+        final_question_list, product_set, y, final_question_text_list, answer_text_list = dagger_get_questions(y,
+                                                                                                    answers_y,
+                                                                                                    model, 
+                                                                                                    question_text_df,
+                                                                                                    answer_text_df,
+                                                                                                    filters_def_dict,
+                                                                                                    products_cat,
+                                                                                                    number_filters)       
     # Save results for MaxMI
-    print('the length of optimal eliminate filter was {}'.format(len(final_question_list)))
+    print('{}: the length of optimal eliminate filter was {}'.format(use, len(final_question_list)))
     opt_prod_nb.append(len(np.unique(product_set["ProductId"])))
     length_opt.append(len(final_question_list))
     opt_quest.append(final_question_list)
@@ -174,7 +211,7 @@ for y in y_array:
     # Save result to external files
     with open(checkpoint_dir +'/lengths.csv', 'a+') as f:
         f.write('{}, {} \n'.format(length_opt[-1], length_rdm[-1]))
-    with open(checkpoint_dir +'/optimal.csv', 'a+') as f:
+    with open(checkpoint_dir +'/{}.csv'.format(use), 'a+') as f:
          f.write('{}, {}, {} \n'.format(opt_quest[-1], opt_quest_text[-1], opt_answer_text_list[-1]))
     with open(checkpoint_dir +'/rdm.csv', 'a+') as f:
          f.write('{}, {}, {} \n'.format(rdm_quest[-1], rdm_quest_text[-1],  rdm_answer_text_list[-1]))
@@ -183,14 +220,14 @@ for y in y_array:
 with open(checkpoint_dir +'/summary.txt', 'w+') as f:
     f.write('Test set size: {} \n Probability of answering I dont know: {} \n Probability of giving 2 answers: {} Probability of giving 3 answers: {} \n'.format(size_test, p_idk, p_2a, p_3a))
     f.write('Alpha history parameter: {}'.format(a_hist))
-    f.write('Avg number of questions for optimal {} \n'.format(np.mean(np.asarray(length_opt))))
-    f.write('Median number of questions for optimal {} \n'.format(np.median(np.asarray(length_opt))))
-    f.write('Std number of questions for optimal {} \n'.format(np.std(np.asarray(length_opt))))
-    f.write('Max number of questions for optimal {} \n'.format(np.max(np.asarray(length_opt))))
-    f.write('Min number of questions for optimal {} \n'.format(np.min(np.asarray(length_opt))))
-    f.write('Max nb of products still left for optimal {} \n'.format(np.max(opt_prod_nb)))
-    f.write('Median nb of products still left for optimal {} \n'.format(np.median(opt_prod_nb)))
-    f.write('Min nb of products still left for optimal {} \n'.format(np.min(opt_prod_nb)))
+    f.write('Avg number of questions for {}: {} \n'.format(use, np.mean(np.asarray(length_opt))))
+    f.write('Median number of questions for {}: {} \n'.format(use,np.median(np.asarray(length_opt))))
+    f.write('Std number of questions for {}: {} \n'.format(use, np.std(np.asarray(length_opt))))
+    f.write('Max number of questions for {}: {} \n'.format(use, np.max(np.asarray(length_opt))))
+    f.write('Min number of questions for {}: {} \n'.format(use, np.min(np.asarray(length_opt))))
+    f.write('Max nb of products still left for {}: {} \n'.format(use, np.max(opt_prod_nb)))
+    f.write('Median nb of products still left for {}: {} \n'.format(use, np.median(opt_prod_nb)))
+    f.write('Min nb of products still left for {}: {} \n'.format(use, np.min(opt_prod_nb)))
     f.write('\n')
     f.write('Avg number of questions for random {} \n'.format(np.mean(np.asarray(length_rdm))))
     f.write('Median number of questions for random {} \n'.format(np.median(np.asarray(length_rdm))))
